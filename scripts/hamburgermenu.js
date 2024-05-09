6c8059bc1e9521228af9a7b26b11f2974f49c714
MaylekArthur98 <Maylek.arthur@gmail.com>
2024-05-09 17:18:59 -0400

    fixed nav hamburger and google maps

--- scripts/hamburgermenu.js
+++ scripts/hamburgermenu.js
@@ -1,7 +1,8 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
-
+const navA = document.getElementById("active")
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
