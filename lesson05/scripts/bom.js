b7f4b8c2f9b916ed1063bbe7d01ec3c6a6e4b503
MaylekArthur98 <Maylek.arthur@gmail.com>
2024-05-25 20:02:23 -0400

    Dom practice

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+button.addEventListener('click', ()=>{
+    if (input.value !=''){
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton ='X';
+        li.apppend(deleteButton);
+        list.append(li);
+        deleteButton.addEventListener('click', function (){
+            list.removeChild(li);
+            input.focus();
+        })
+        input.focus();
+        input.value='';
+    }
+    else {
+        return `please enter valid input`
+    }
+ });
