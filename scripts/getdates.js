27592399687a3b5853ec12772804e84c36b3fc3a
MaylekArthur98 <Maylek.arthur@gmail.com>
2024-06-20 21:30:10 -0400

    finished work

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,15 @@ const year = document.getElementById('year');
 year.innerHTML= `&#169; ${new Date().getFullYear()}`;
 const lastModified = document.getElementById('lastModified');
 lastModified.innerHTML = document.lastModified;
+const visitsDisplay = document.querySelector(".visits");
+
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. Welcome!`;
+}
+
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
