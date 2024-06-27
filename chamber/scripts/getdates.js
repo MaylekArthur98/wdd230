183b49fd085bafb38a413180d0b199e1717e8b3a
MaylekArthur98 <Maylek.arthur@gmail.com>
2024-06-26 22:06:35 -0400

    finished adding shadow effects

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -3,23 +3,21 @@ year.innerHTML= `&#169; ${new Date().getFullYear()}`;
 const lastModified = document.getElementById('lastModified');
 lastModified.innerHTML = document.lastModified;
 
-const visitsDisplay = document.getElementById("visits");
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+const visitDisplay = document.getElementById("visit");
+let visit = Number(window.localStorage.getItem("lastVisit-ls")) || 0;
 const msToDays = 84600000;
-let daysBetween = ((Date.now()- numVisits) / msToDays).toFixed(0);
+let days = ((Date.now() - visit) / msToDays).toFixed(0);
 
-if (numVisits == 0){
-    visitsDisplay.textContent = `Welcome! let us know if you have any questions.`;
-}
-else if ((Date.now() - numVisits) < msToDays){
-    visitsDisplay.textContent = `Back so soon! Awesome!`;
-}
-else if (daysBetween == 1){
-    `You last visited 1 day ago`;
-}
-else {
+if (visit == 0) {
+    visitDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+} else if ((Date.now() - visit) < msToDays) {
+    visitDisplay.textContent = `Back so soon! Awesome!`;
+} else if (days == 1) {
+    `You last visited 1 day ago.`
+} else {
     `You last visited ${days} days ago.`;
 }
-numVisits = Date.now();
 
-localStorage.setItem("numVisits-ls", numVisits);
+visit = Date.now()
+
+localStorage.setItem("lastVisit-ls", visit);
