735a1fc96d2ae568fcbb72ecc2c38cbb38de271d
MaylekArthur98 <Maylek.arthur@gmail.com>
2024-06-26 21:15:47 -0400

    did a lot of work

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,4 +1,25 @@
 const year = document.getElementById('year');
-year.innerHTML= `&#169;  Silver Spring Chamber ${new Date().getFullYear()}`;
+year.innerHTML= `&#169; ${new Date().getFullYear()}`;
 const lastModified = document.getElementById('lastModified');
 lastModified.innerHTML = document.lastModified;
+
+const visitsDisplay = document.getElementById(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+const msToDays = 84600000;
+let daysBetween = ((Date.now()- numVisits) / msToDays).toFixed(0);
+
+if (numVisits == 0){
+    visitsDisplay.textContent = `Welcome! let us know if you have any questions.`;
+}
+else if ((Date.now() - numVisits) < msToDays){
+    visitsDisplay.textContent = `Back so soon! Awesome!`;
+}
+else if (daysBetween == 1){
+    `You last visited 1 day ago`;
+}
+else {
+    `You last visited ${days} days ago.`;
+}
+numVisits = Date.now();
+
+localStorage.setItem("numVisits-ls", numVisits);
