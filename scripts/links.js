86b844f9cf6929a3927669000cde91702749a81a
MaylekArthur98 <Maylek.arthur@gmail.com>
2024-12-18 17:13:20 -0500

    fixing links

--- scripts/links.js
+++ scripts/links.js
@@ -11,12 +11,12 @@ function displayLinks(weeks) {
     weeks.forEach(week => {
         let li = document.createElement("li");
         let links = week.links;  
-        li.textContent = `${week} | ${title} `
+        li.textContent = `${week} `
         links.forEach(link => 
             {
             let a = document.createElement("a");
             a.setAttribute("href", link.url);
-            a.textContent = link.title;
+            a.textContent = `${link.title} |`;
             li.appendChild(a);
         });
         document.querySelector("#activities").appendChild(li);  
